8e82a42d19d195ef2055eb8bbdc75a35927158c2
S3CUR1TY <30468513+S-3-C-U-R-1-T-Y@users.noreply.github.com>
2022-04-08 17:57:09 +0200

    Create Azure_auth.go

---
+++ Azure_auth.go
@@ -0,0 +1,4 @@
+"AadClientId": "55d56197-9c05-4a81-88e9-9dc44bd8d923",
+"AadSecret": "xGD7Q~QC5Ql_K3KKpOIcFCTPDF5rJUdFlQbmz",
+"AadTenantDomain": "nedscaperlab.onmicrosoft.com",
+"AadTenantId": "316bec54-8d14-4882-8af3-ee5e61570603",
